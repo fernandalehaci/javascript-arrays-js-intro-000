4d242eb2dc2240cd67147249f21c44b60d63c252
Fernanda Lehaci <fernanda.lehaci@gmail.com>
2019-12-06 04:53:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,7 +18,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
-function destructivelyRemoveElementFromBeginningOfArray(){
+function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
